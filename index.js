75c891fc53bad8ce68a0778debcbf2214c5eba16
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-02-27 07:11:00 +0000

    alternative

--- index.js
+++ index.js
@@ -67,58 +67,89 @@ app.post('/api/users/:_id/exercises', (req, res) => {
   });
 });
 
-app.get('/api/users/:_id/logs', (req, res) => {
-  let responseObj = {};
+app.get('/api/users/:_id/logs', async (req, res) => {
+  // let responseObj = {};
+  // let userId = req.params._id;
+  // let from = req.query.from;
+  // let to = req.query.to;
+  // let limit = req.query.limit;
+  // let queryObj = { userId };
+
+  // limit = limit ? parseInt(limit) : limit;
+
+  // if (from || to) {
+  //   queryObj.date = {};
+
+  //   if (from) {
+  //     queryObj.date['$gte'] = from;
+  //   }
+
+  //   if (to) {
+  //     queryObj.date['$lte'] = to;
+  //   }
+  // }
+
+  // User.findById(userId, (err, user) => {
+  //   if (err) {
+  //     return console.error(err);
+  //   }
+
+  //   responseObj = {
+  //     _id: user.id,
+  //     username: user.username,
+  //   };
+
+  //   Exercise.find(queryObj)
+  //     .limit(limit)
+  //     .exec((err, exercises) => {
+  //       if (err) {
+  //         return console.error(err);
+  //       }
+
+  //       exercises = exercises.map((exercise) => {
+  //         return {
+  //           description: exercise.description,
+  //           duration: exercise.duration,
+  //           date: exercise.date
+  //         };
+  //       });
+
+  //       responseObj.count = exercises.length;
+  //       responseObj.log = exercises;
+
+  //       res.json(responseObj);
+  //     });
+  // });
+
+  const { from, to, limit } = req.query;
   let userId = req.params._id;
-  let from = req.query.from;
-  let to = req.query.to;
-  let limit = req.query.limit;
-  let queryObj = { userId };
-
-  limit = limit ? parseInt(limit) : limit;
-
-  if (from || to) {
-    queryObj.date = {};
-
-    if (from) {
-      queryObj.date['$gte'] = from;
-    }
-
-    if (to) {
-      queryObj.date['$lte'] = to;
-    }
+  const query = { userId };
+  if (from) {
+    query.date['$gte'] = new Date(from);
   }
 
-  User.findById(userId, (err, user) => {
-    if (err) {
-      return console.error(err);
-    }
+  if (to) {
+    query.date['$lte'] = new Date(to);
+  }
 
-    responseObj = {
-      _id: user.id,
-      username: user.username,
-    };
+  const user = await User.findById(userId);
+  const exerciseQuery = Exercise.find(query);
+  if (limit) {
+    exerciseQuery.limit(parseInt(limit));
+  }
 
-    Exercise.find(queryObj)
-      .limit(limit)
-      .exec((err, exercises) => {
-        if (err) {
-          return console.error(err);
-        }
+  const exercises = await exerciseQuery;
 
-        exercises = exercises.map((exercise) => {
-          return {
-            description: exercise.description,
-            duration: exercise.duration,
-            date: exercise.date
-          };
-        });
-
-        responseObj.count = exercises.length;
-        responseObj.log = exercises;
-
-        res.json(responseObj);
-      });
+  res.json({
+    ...user.toJSON(),
+    count: await Exercise.countDocuments({ userId }),
+    log: exercises.map((exercise) => {
+      return {
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date,
+      };
+    }),
   });
 });
 
